fda55a1cb121c78e259057c32363de9098dcd8cf
Jason Kadman <jkadman231@gmail.com>
2023-01-23 06:18:06 +0000

    setup defence against sql injection

--- bootcamp_app/teachers.js
+++ bootcamp_app/teachers.js
@@ -8,16 +8,17 @@ const pool = new Pool({
 });
 
 const cohortName = process.argv[2];
-
-pool.query(`
-SELECT DISTINCT teachers.name as teacher, cohorts.name as cohort
+const values = ['%${cohortName}%']
+const text = `SELECT DISTINCT teachers.name as teacher, cohorts.name as cohort
 FROM teachers
 JOIN assistance_requests ON teacher_id = teachers.id
 JOIN students ON student_id = students.id
 JOIN cohorts ON cohort_id = cohorts.id
-WHERE cohorts.name LIKE '%${cohortName}%'
+WHERE cohorts.name LIKE $1
 ORDER BY teacher;
-`)
+`;
+
+pool.query(text, values)
 .then(res => {
     res.rows.forEach(element => {
       console.log(`${cohortName}: ${element.teacher}`)
